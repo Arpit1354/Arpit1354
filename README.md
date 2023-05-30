a9a922a704787ac75707b33cea6b731283bc1562
Arpit1354 <134362571+Arpit1354@users.noreply.github.com>
2023-05-30 17:37:37 +0530

    Spotify Clone

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-- 👋 Hi, I’m @Arpit1354
-- 👀 I’m interested in Software Development
-- 🌱 I’m currently learning DSA
-- 📫 How to reach me Phone Number:- 8219364679 ,Gmail 82193work@gmail.com
+# [Global: HTML5 website template](http://buckymaler.com/global)
 
-<!---
-Arpit1354/Arpit1354 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+### [Featured on Freebiesbug](http://freebiesbug.com/psd-freebies/global-futuristic-one-page-portfolio-psd-html/)
+
+Global is a modern and sleek website template crafted for professionals in the creative industry. Global can serve as a single page portfolio or be easily modified to serve as the perfect beginning for a hip creative agency.
+
+The original PSD is included and was provided by [Sergey Melnik](https://www.behance.net/SergeyMelnik).
+
+This project was developed with HTML5, Sass, jQuery, and Hammer.js.
+
+Performance basics are covered: assets are minified into single CSS and JS files, and the images are optimized.
+
+[LIVE PREVIEW](http://buckymaler.com/global)
+
+## Misc:
+
+* Follow Bucky: [Twitter](https://twitter.com/BuckyMaler), [GitHub](https://github.com/BuckyMaler)
+* Follow Sergey: [Dribbble](https://dribbble.com/sergeymelnik), [Bēhance](https://www.behance.net/SergeyMelnik)
---
+++ app.js
@@ -0,0 +1,60 @@
+// var arpit = true;
+// console.log(arpit)
+// practice
+console.log(10+"20");
+console.log(10-"2")//bug
+console.log("arpit"+"sharma");
+console.log(""+0);
+console.log("arpit"-"sharma");
+console.log(true+true)
+console.log(true-true)
+console.log(true+false)
+console.log(true-false)
+// interview question
+// null vs undefined
+// NUll = there is no variable present
+// undefined = the variable is present but there is no value in it
+// var arppit =null;
+// console.log(typeof(arppit)) //bug
+// var arppit 
+// console.log((arppit))
+// What is NAN
+// In other words , it is a variable in global scope
+// the initial valie of NAN is not a Number
+var phonenumber =219364679;
+var myname ="arpit";
+console.log(219364679-"arpit");
+console.log(isNaN(phonenumber));
+if(isNaN(myname)){
+    console.log("not a valid number");
+}
+// practice set
+console.log(NaN==NaN)
+// arithmentic
+console.log(5+10)
+// what is expression
+// combination of operator and operands is called expression
+// types of operators
+// 6types of operator
+// An assignmnent operator assign a value to its left operand
+//  ANOTHER BUG
+// a=5;
+// b=5;
+// console.log("both a and b are equal or not"+a==b)
+// console.log(a==b);
+// arithmetic operator
+console.log(1+2)
+console.log(2-1);
+console.log(2*2);
+console.log(2/2);
+// increment decrement
+// inrement is used to increase the value
+// decrement is used to decrease the value
+//pre increment first increase the value 
+//example pre
+var num =1;
+var newnum = num++;
+console.log(num);
+
+// comparison operator
+console.log(9-"2")
